09c12dfa0cf771e8018a80dac90419068962437f
Vpyc <vpyc4408@gmail.com>
2024-05-21 14:15:45 +0300

    change library for BarChart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
             )
         }
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +39,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
     packaging {
         resources.excludes.add("META-INF/DEPENDENCIES")
@@ -47,7 +51,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
     ksp("androidx.room:room-compiler:2.6.1")
 
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("co.yml:ycharts:2.1.0")
+
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.compiler:compiler:1.5.14")
 
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
     implementation("androidx.core:core-ktx:1.13.1")
@@ -60,4 +73,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/example/myfinapp/ui/chart/ChartFragment.kt
+++ app/src/main/java/com/example/myfinapp/ui/chart/ChartFragment.kt
@@ -1,21 +1,36 @@
 package com.example.myfinapp.ui.chart
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.fragment.app.Fragment
 import com.example.myfinapp.databinding.FragmentChartBinding
 import com.example.myfinapp.room.McsItem
-import com.github.mikephil.charting.charts.BarChart
-import com.github.mikephil.charting.components.XAxis
+/*import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import java.text.SimpleDateFormat
-import java.util.Locale
+import com.github.mikephil.charting.data.BarEntry*/
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import co.yml.charts.axis.AxisData
+import co.yml.charts.axis.DataCategoryOptions
+import co.yml.charts.common.model.Point
+import co.yml.charts.common.utils.DataUtils
+import co.yml.charts.ui.barchart.BarChart
+import co.yml.charts.ui.barchart.models.BarChartData
+import co.yml.charts.ui.barchart.models.BarChartType
+import co.yml.charts.ui.barchart.models.BarData
+import com.example.myfinapp.R
 
 class ChartFragment : Fragment() {
     private var _binding: FragmentChartBinding? = null
@@ -29,65 +44,103 @@ class ChartFragment : Fragment() {
     ): View {
 
         _binding = FragmentChartBinding.inflate(inflater, container, false)
+//        val view =
+        /*binding.composeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                // In Compose world
+                MaterialTheme {
+                    Text("Hello Compose!")
+                }
+            }
+        }*/
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mcs = arguments?.getParcelableArrayList<McsItem>("mcs")
-
-        val barChart = binding.barChart
-
-        val barData = mcs?.let { createBarData(it, barChart) }
-
-        barChart.data = barData
-        barChart.invalidate()
+        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+        composeView.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                if (!mcs.isNullOrEmpty()){
+                    McsBarChart(mcs)
+                }
+            }
+        }
     }
 
-    private fun createBarData(items: List<McsItem>, barChart: BarChart): BarData {
-        val dateFormat = SimpleDateFormat("MM.yyyy", Locale.getDefault())
+    @Composable
+    fun McsBarChart(mcs: ArrayList<McsItem>) {
+        val stepSize = 5
+        val barsData = createMcsBarChartData(mcs)/*DataUtils.getBarChartData(
+            listSize = 8,
+            maxRange = 8,
+            barChartType = BarChartType.VERTICAL,
+            dataCategoryOptions = DataCategoryOptions()
+        )*/
+        val xAxisData = AxisData.Builder()
+            .axisStepSize(100.dp)
+            .steps(barsData.size - 1)
+            .bottomPadding(40.dp)
+            .axisLabelAngle(20f)
+            .labelData {index -> barsData[index].label}
+            .axisLineColor(MaterialTheme.colorScheme.tertiary)
+            .axisLabelColor(MaterialTheme.colorScheme.tertiary)
+            .build()
+        val yAxisData = AxisData.Builder()
+            .steps(stepSize)
+            .labelAndAxisLinePadding(20.dp)
+            .axisOffset(20.dp)
+            .labelData {index -> (index * (100 /stepSize)).toString()}
+            .axisLineColor(MaterialTheme.colorScheme.tertiary)
+            .axisLabelColor(MaterialTheme.colorScheme.tertiary)
+            .build()
+        val barChartData = BarChartData(
+            chartData = barsData,
+            xAxisData = xAxisData,
+            yAxisData = yAxisData,
+            backgroundColor = MaterialTheme.colorScheme.surface
+        )
+        BarChart(
+            modifier = Modifier
+                .height(250.dp),
+            barChartData = barChartData
+        )
+    }
+    fun createMcsBarChartData(mcs: ArrayList<McsItem>): List<BarData> {
+        val barDataList = arrayListOf<BarData>()
 
-        val plusEntries = mutableListOf<BarEntry>()
-        val minusEntries = mutableListOf<BarEntry>()
+        for (index in mcs.indices) {
+            val item = mcs[index]
+            val plusValue = item.plus.toFloat()
+            val minusValue = item.minus.toFloat() * -1 // Преобразуем минусы
 
-        val dates = items.map { it.formattedDate }
-        val uniqueDates = dates.distinct()
+            val point = Point(
+                index.toFloat(),
+                plusValue
+            )
 
-        uniqueDates.forEachIndexed { index, date ->
-            val plusValue = items.filter { it.formattedDate == date }.sumByDouble { it.plus }
-            val minusValue = items.filter { it.formattedDate == date }.sumByDouble { it.minus }
+            val barData = item.formattedDate?.let {
+                BarData(
+                    point = point,
+                    color = if (plusValue >= 0) Color.Green else Color.Red, // Зеленый для плюсов, красный для минусов
+                    dataCategoryOptions = DataCategoryOptions(),
+                    label = it
+                )
+            }
 
-            plusEntries.add(BarEntry(index.toFloat(), plusValue.toFloat()))
-            minusEntries.add(BarEntry(index.toFloat(), minusValue.toFloat() * -1))
+            if (barData != null) {
+                barDataList.add(barData)
+            }
         }
 
-        val plusDataSet = BarDataSet(plusEntries, "Плюсы")
-        plusDataSet.color = Color.GREEN
-        plusDataSet.setDrawValues(false)
-
-        val minusDataSet = BarDataSet(minusEntries, "Минусы")
-        minusDataSet.color = Color.RED
-        minusDataSet.setDrawValues(false)
-
-        val barWidth = 0.4f
-
-        val barData = BarData(plusDataSet, minusDataSet)
-        barData.barWidth = barWidth
-
-        val groupCount = uniqueDates.size
-        val startYear = 0
-
-        barChart.xAxis.axisMinimum = startYear.toFloat()
-        barChart.xAxis.axisMaximum = startYear + groupCount.toFloat()
-        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(uniqueDates)
-        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-        barChart.xAxis.setDrawGridLines(false)
-        barChart.setDrawGridBackground(false)
-        barChart.axisRight.isEnabled = false
-        barChart.xAxis.setLabelCount(uniqueDates.size, true)
-
-        return barData
+        return barDataList
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
